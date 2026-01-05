2d612abbf87be9653d81c7909ac8ce0c45153726
satish28 <satish28888@gmail.com>
2026-01-05 06:54:19 +0530

    Refactor file read

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ For identified services what give references to what type of attacks can be test
 """
 sec_engineer = """I am a Senior Network Security Engineer, in charge of securing my org’s internal and external networks."""
 
+def read_file(nmap_file):
+    with open(nmap_file, "r") as f:
+        return f.read()
+
 def ai_llm_integrtation(nmap_file, special_prompt, model):
     
     llm_prompt = sec_engineer + nmap_file + special_prompt
@@ -86,9 +90,7 @@ def analyse_cli(ctx, nmap_file):
 @click.pass_context
 def cve_scan(ctx, nmap_file):
     """Scans for CVE vulnerabilities."""
-    with open(nmap_file, "r") as f:
-        read_nmap_file = f.read()
-    
+    read_nmap_file  = read_file(nmap_file)
     model = ctx.obj["model"]
     cve_finder(read_nmap_file, model)
 
@@ -97,8 +99,7 @@ def cve_scan(ctx, nmap_file):
 @click.pass_context
 def exploit_scan(ctx, nmap_file):
     """Attempts to exploit identified vulnerabilities."""
-    with open(nmap_file, "r") as f:
-        read_nmap_file = f.read()
+    read_nmap_file  = read_file(nmap_file)
     model = ctx.obj["model"]
     exploit_finder(read_nmap_file, model)
 
@@ -107,8 +108,7 @@ def exploit_scan(ctx, nmap_file):
 @click.pass_context
 def assist_scan(ctx, nmap_file):
     """Provides assistance and guidance based on scan results."""
-    with open(nmap_file, "r") as f:
-        read_nmap_file = f.read()
+    read_nmap_file  = read_file(nmap_file)
     model = ctx.obj["model"]
     share_test_plan(read_nmap_file, model)
 
